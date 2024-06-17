44018891cb85d99f47b819f296c9a510141144e3
kazibilsi <kazibilasi@gmail.com>
2024-06-17 10:18:43 +0600

    add product end point work has done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const port = 4000;
+const port = 5000;
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken');
 const multer = require('multer')
 const path = require('path')
 const cors = require('cors');
+
 
 
 app.use(express.json())
@@ -20,10 +21,84 @@ app.get("/", (req, res)=>{
     res.send('Express App is Running')
 })
 
+// image storage engine
+
+const storage = multer.diskStorage({
+    destination : './upload/images',
+    filename:(req,file,cb)=>{
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}}`)
+    }
+})
+
+const upload = multer({storage:storage})
+// creating upload endpoint for images
+app.use('/images', express.static('upload/images'))
+app.post('/upload', upload.single('product'),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+// Schema for creating Products
+const Product = mongoose.model("Product", {
+   id:{
+    type: Number,
+    required: true,
+   } ,
+   name:{
+    type:String,
+    required:true,
+   },
+   image:{
+    type:String,
+    required:true,
+   },
+   category:{
+    type:String,
+    required:true,
+   },
+   new_price:{
+    type:Number,
+    required:true,
+   },
+   old_price:{
+    type:Number,
+    required:true,
+   },
+   date:{
+    type:Date,
+    default:Date.now,
+   },
+   available:{
+    type: Boolean,
+    default:true,
+   }
+})
+
+
+app.post('/addProduct', async (req, res)=>{
+    const product = new Product({
+        id:req.body.id,
+        name: req.body.name,
+        image:req.body.image,
+        category: req.body.category,
+        new_price: req.body.new_price,
+        old_price:req.body.old_price
+    })
+    console.log(product)
+    await product.save()
+    console.log('Saved')
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+
 app.listen(port,(error)=>{
     if(!error){
-        console.log("Server Running on Port" + port)
+        console.log("Server Running on Port" +  port)
     }else{
-        console.log("Error: "+error)
+        console.log("Error: " +error)
     }
 })
